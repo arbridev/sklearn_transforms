4ecb5d6d5a655e8eb15e9f4ab6e2fc48fe58d03c
arbridev <arebrif@gmail.com>
2020-08-18 22:45:55 -0400

    add DataFrameImputer for missing values

--- my_custom_sklearn_transforms/sklearn_transformers.py
+++ my_custom_sklearn_transforms/sklearn_transformers.py
@@ -1,4 +1,6 @@
 from sklearn.base import BaseEstimator, TransformerMixin
+import pandas as pd
+import numpy as np
 
 
 # All sklearn Transforms must have the `transform` and `fit` methods
@@ -14,3 +16,29 @@ class DropColumns(BaseEstimator, TransformerMixin):
         data = X.copy()
         # Retornamos um novo dataframe sem as colunas indesejadas
         return data.drop(labels=self.columns, axis='columns')
+
+
+# Imputes missing values with mean or most frequent
+# https://stackoverflow.com/questions/25239958/impute-categorical-missing-values-in-scikit-learn
+class DataFrameImputer(TransformerMixin):
+    def __init__(self):
+        """Impute missing values.
+
+        Columns of dtype object are imputed with the most frequent value 
+        in column.
+
+        Columns of other types are imputed with mean of column.
+
+        """
+    def fit(self, X, y=None):
+        self.fill = pd.Series(
+            [
+                X[c].value_counts().index[0]
+                    if X[c].dtype == np.dtype('O') else X[c].mean() for c in X
+            ],
+            index=X.columns
+            )
+        return self
+
+    def transform(self, X, y=None):
+        return X.fillna(self.fill)
